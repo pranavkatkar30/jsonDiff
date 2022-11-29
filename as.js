eee2b91ea5b64d7f0a8b5186c38d1a901a9a2710
Pranav Katkar <pranavkatkar@ip-172-16-2-79.ap-south-1.compute.internal>
2022-11-29 18:05:21 +0530

    tell difference between 2 json files

---
+++ as.js
@@ -0,0 +1,254 @@
+
+//Bhupendra_B2C.json
+var myObj1= {
+    "_class": "com.turtlemint.mintprobe.model.partnerDetails.Partner",
+    "_id": "62c6ce26ff74520001ce00ba",
+    "acquisitionOwner": null,
+    "address": {
+        "city": "Betul",
+        "country": "India",
+        "currentPincode": "460551",
+        "displayAddress": "H No 229",
+        "pincode": "460551",
+        "state": "Madhya Pradesh"
+    },
+    "allowAccess": true,
+    "associateActive": false,
+    "associatePartnerStatus": false,
+    "bankDetails": {
+        "relationship": "self",
+        "validationRetries": 10,
+        "verified": false
+    },
+    "bankDetailsVerifier": {
+        "verifierDpNo": 0
+    },
+    "branchCode": "1000",
+    "businessChannel": "notset",
+    "businessProfile": {
+        "customPosterStripId": "1c57d5d5-4121-4da3-b609-83fa70a5bc3d"
+    },
+    "businessVertical": "NA",
+    "category": "partner",
+    "certificateDetails": [
+        {
+            "assignAsParent": false,
+            "pospType": "na"
+        }
+    ],
+    "completed": false,
+    "createdAt": "2022-07-07T12:14:30.062Z",
+    "deactivatedBy": "sachin.pangam@turtlemint.com",
+    "deactivationDate": "2022-07-07T12:20:45.766Z",
+    "deactivationReason": "Partner not interested to work",
+    "documentNews": [
+        {
+            "angle": 0,
+            "type": "pan_card"
+        },
+        {
+            "angle": 0,
+            "type": "profile_picture"
+        },
+        {
+            "angle": 0,
+            "type": "CERTIFICATE"
+        },
+        {
+            "angle": 0,
+            "type": "certificate_li"
+        }
+    ],
+    "documentOwner": true,
+    "dpNo": 1555,
+    "educationQualification": null,
+    "email": "ZCwdGqZ9hl5lkeh+rGA5fVNMAhmSdSE3IjHx0vk0e88jiiAj53G0IQ==",
+    "existingBusinessTurnover": null,
+    "gender": "male",
+    "hasNocCertificate": false,
+    "hasTMAdminUser": false,
+    "insuranceExperienceInYears": null,
+    "irdaiStatus": "not_checked",
+    "isActivationTriggered": null,
+    "isEligibleForLMS": true,
+    "isLoginAllowed": false,
+    "leadAcquisition": {
+        "_id": "62c6ce26ff74520001ce00b9",
+        "createdAt": "2022-07-07T12:14:30.777Z",
+        "dpNo": 1555,
+        "saleStatus": "new",
+        "searchKeys": "",
+        "updatedAt": "2022-07-07T12:14:30.867Z"
+    },
+    "licenseDetails": [],
+    "lms": null,
+    "lmsEnrollmentDate": "2022-07-15T07:03:49.755Z",
+    "maritalStatus": false,
+    "mobile": "pq7JHLUt6AlsUHOXQoQtXQ==",
+    "monthlyHealthPremium": null,
+    "monthlyLifePremium": null,
+    "monthlyMotorPremium": null,
+    "name": "bhupendra",
+    "nameAlias1": "bhupendra wadekar",
+    "nameAlias2": "bhupendra wadekar",
+    "nomineeBankDetails": {
+        "validationRetries": 10,
+        "verified": false
+    },
+    "officeSpace": false,
+    "oldAssociatedPOSPId": [],
+    "oldParents": [],
+    "oldPartnerNewRefs": [],
+    "oldReportees": [],
+    "oldSupervisors": [],
+    "organisation": "ippbbc",
+    "partnerNewRefs": [],
+    "partnerQc": {},
+    "partnerType": "partner_level_1",
+    "partnerVerifier": {
+        "verifierDpNo": 0
+    },
+    "password": null,
+    "passwordLoginEnabled": false,
+    "pospDetail": {
+        "assignAsParent": false,
+        "expiryDate": "2023-07-05T18:30:00Z",
+        "licenseNumber": "IPPB/POSP/GI/786",
+        "licenseStartDate": "2020-07-05T18:30:00Z"
+    },
+    "pospInsurerDetails": [],
+    "pospType": "na",
+    "primarySourceOfIncome": null,
+    "profileValidateSave": true,
+    "referralCode": "ASH0001555",
+    "registeredAtIrda": false,
+    "reportees": [],
+    "salesStatus": "agreedtopartner",
+    "source": "upload",
+    "status": "verified",
+    "subscriptionType": "REGULAR",
+    "tenantUniqueCode": "T106971412",
+    "transactionInfo": null,
+    "updatedAt": "2022-11-10T12:33:41.388Z",
+    "userType": "login",
+    "username": "62c6ce26ff74520001ce00ba",
+    "verificationDate": null
+};
+var myObj2 = {
+    "_class": "com.turtlemint.mintprobe.model.partnerDetails.Partner",
+    "_id": "636bca76a77015000138e59d",
+    "acquisitionOwner": null,
+    "address": null,
+    "allowAccess": true,
+    "associateActive": false,
+    "associatePartnerStatus": false,
+    "bankDetails": null,
+    "bankDetailsVerifier": null,
+    "branchCode": "345",
+    "businessChannel": "notset",
+    "businessProfile": {
+        "customPosterStripId": "1edd75d9-567c-433f-934d-c1ee55c1750b"
+    },
+    "businessVertical": "NA",
+    "category": "partner",
+    "certificateDetails": null,
+    "completed": false,
+    "createdAt": "2022-11-09T15:42:46.111Z",
+    "deactivatedBy": null,
+    "deactivationDate": null,
+    "deactivationReason": null,
+    "documentNews": [],
+    "documentOwner": true,
+    "dpNo": 4673,
+    "educationQualification": null,
+    "email": "NbDjxIDnnTQPK2K5VYUdB+KWvTYs7KRSWetWSQ/qojI=",
+    "existingBusinessTurnover": null,
+    "gender": null,
+    "hasNocCertificate": false,
+    "hasTMAdminUser": false,
+    "insuranceExperienceInYears": null,
+    "irdaiStatus": "not_checked",
+    "isActivationTriggered": null,
+    "isEligibleForLMS": null,
+    "isLoginAllowed": false,
+    "leadAcquisition": {
+        "_id": "636bca76a77015000138e59c",
+        "createdAt": "2022-11-09T15:42:46.203Z",
+        "dpNo": 4673,
+        "saleStatus": "new",
+        "searchKeys": "",
+        "updatedAt": "2022-11-09T15:42:46.203Z"
+    },
+    "licenseDetails": [],
+    "lms": null,
+    "lmsEnrollmentDate": null,
+    "maritalStatus": false,
+    "mobile": "KclsxeV6oXmdQpDVIBvBDQ==",
+    "monthlyHealthPremium": null,
+    "monthlyLifePremium": null,
+    "monthlyMotorPremium": null,
+    "name": "nilesh netaji jadhav",
+    "nameAlias1": null,
+    "nameAlias2": null,
+    "nomineeBankDetails": null,
+    "officeSpace": false,
+    "oldAssociatedPOSPId": [],
+    "oldParents": [],
+    "oldPartnerNewRefs": [],
+    "oldReportees": [],
+    "oldSupervisors": [],
+    "organisation": "ippbbc",
+    "partnerNewRefs": [],
+    "partnerQc": null,
+    "partnerType": "partner_level_1",
+    "partnerVerifier": null,
+    "password": null,
+    "passwordLoginEnabled": false,
+    "pospDetail": {
+        "assignAsParent": false,
+        "expiryDate": "2022-11-05T18:30:00Z",
+        "licenseNumber": "IPPB/POSP/GI/227138",
+        "licenseStartDate": "2022-11-06T18:30:00Z"
+    },
+    "pospInsurerDetails": [],
+    "pospType": "na",
+    "primarySourceOfIncome": null,
+    "profileValidateSave": true,
+    "referralCode": "NIL0004673",
+    "registeredAtIrda": false,
+    "reportees": [],
+    "salesStatus": "new",
+    "source": "upload",
+    "status": "verified",
+    "subscriptionType": "REGULAR",
+    "tenantUniqueCode": "IPPBMAH07969",
+    "transactionInfo": null,
+    "updatedAt": "2022-11-10T07:29:29.123Z",
+    "userType": "login",
+    "username": "636bca76a77015000138e59d",
+    "verificationDate": null
+};
+// find keys
+keyObj1 = Object.keys(myObj1); 
+	keyObj2 = Object.keys(myObj2); 
+		 
+	// find values 
+	valueObj1 = Object.values(myObj1); 
+	valueObj2 = Object.values(myObj2); 
+	 
+	// find max length to iterate	 
+	if(keyObj1.length > keyObj2.length) { 
+		var biggestKey = keyObj1.length; 
+	} else { 
+		var biggestKey = keyObj2.length; 
+	} 
+	 
+	// now compare their keys and values  
+	for(var i=0; i<biggestKey; i++) { 
+		if(keyObj1[i] == keyObj2[i] && valueObj1[i] == valueObj2[i]) { 
+			console.log(valueObj2[i]);	 
+		} else { 
+			// it prints keys have different values 
+			console.log('myObj1 value: '+ valueObj1[i] + '\nmyObj2 value: '+ valueObj2[i] +'\n'); 
+		} 
+	}	
